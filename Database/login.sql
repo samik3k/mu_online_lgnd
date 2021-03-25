ef8bd6a94467e03ef4e9f292ab382c6f187016ce
Alexander Sokolov <a.sokolov@runexis.ru>
2021-03-26 01:37:54 +0300

    Updated db

--- Database/login.sql
+++ Database/login.sql
@@ -24,10 +24,10 @@ DROP TABLE IF EXISTS `accounts`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `accounts` (
   `guid` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `blocked` tinyint(4) unsigned NOT NULL,
+  `blocked` tinyint(4) unsigned NOT NULL DEFAULT '0',
   `security_code` varchar(255) DEFAULT NULL,
   `golden_channel` bigint(20) NOT NULL,
-  `facebook_status` tinyint(4) unsigned NOT NULL,
+  `facebook_status` tinyint(4) unsigned NOT NULL DEFAULT '0',
   `secured` tinyint(4) unsigned NOT NULL,
   `account` varchar(255) NOT NULL,
   `password` varchar(255) DEFAULT NULL,
@@ -35,7 +35,7 @@ CREATE TABLE `accounts` (
   `register` bigint(20) NOT NULL,
   PRIMARY KEY (`guid`),
   UNIQUE KEY `account_UNIQUE` (`account`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,7 +44,7 @@ CREATE TABLE `accounts` (
 
 LOCK TABLES `accounts` WRITE;
 /*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
-INSERT INTO `accounts` VALUES (1,0,'12digitsphra',1500434821,0,1,'user','67eabbf39d1e39ae7fad930244949c85d12b72965795794c9d5b66e8d8595467','user@email.com',20210311111308),(2,0,'12digitsphra',1500434821,0,1,'user2','ae471a7d82020498f9bab02f476d40e61bbdd985e1d625825a7ed603be8cb498','user2@email.com',20210312134729);
+INSERT INTO `accounts` VALUES (1,0,'12digitsphra',1500434821,0,1,'user','67eabbf39d1e39ae7fad930244949c85d12b72965795794c9d5b66e8d8595467','user@email.com',20210311111308),(2,0,'12digitsphra',1500434821,0,1,'user2','ae471a7d82020498f9bab02f476d40e61bbdd985e1d625825a7ed603be8cb498','user2@email.com',20210312134729),(4,0,'12digitsphra',1500434821,0,1,'test','0abf9601b8ad64ec7c88814ecb7b7ee6da1b54e74aaf6393c4e8ac37a91f2489','test@email.com',20210325202638);
 /*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -351,7 +351,7 @@ CREATE TABLE `item_serial` (
 
 LOCK TABLES `item_serial` WRITE;
 /*!40000 ALTER TABLE `item_serial` DISABLE KEYS */;
-INSERT INTO `item_serial` VALUES (0,0,0);
+INSERT INTO `item_serial` VALUES (0,129,0);
 /*!40000 ALTER TABLE `item_serial` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -421,7 +421,7 @@ DROP TABLE IF EXISTS `world_server`;
 CREATE TABLE `world_server` (
   `server` smallint(6) unsigned NOT NULL,
   `world` smallint(6) unsigned NOT NULL,
-  PRIMARY KEY (`server`)
+  PRIMARY KEY (`server`,`world`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -431,7 +431,7 @@ CREATE TABLE `world_server` (
 
 LOCK TABLES `world_server` WRITE;
 /*!40000 ALTER TABLE `world_server` DISABLE KEYS */;
-INSERT INTO `world_server` VALUES (0,0);
+INSERT INTO `world_server` VALUES (0,0),(0,1),(0,2),(0,3),(0,4),(0,5),(0,6),(0,7),(0,8),(0,9),(0,10),(0,11),(0,12),(0,13),(0,14),(0,15),(0,16),(0,17),(0,18),(0,19),(0,20),(0,21),(0,22),(0,23),(0,24),(0,25),(0,26),(0,27),(0,28),(0,29),(0,30),(0,31),(0,32),(0,33),(0,34),(0,35),(0,36),(0,37),(0,38),(0,39),(0,40),(0,41),(0,42),(0,43),(0,44),(0,45),(0,46),(0,47),(0,48),(0,49),(0,50),(0,51),(0,52),(0,53),(0,54),(0,55),(0,56),(0,57),(0,58),(0,59),(0,60),(0,61),(0,62),(0,63),(0,64),(0,65),(0,66),(0,67),(0,68),(0,69),(0,70),(0,71),(0,72),(0,73),(0,74),(0,75),(0,76),(0,77),(0,78),(0,79),(0,80),(0,81),(0,82),(0,83),(0,84),(0,85),(0,86),(0,87),(0,88),(0,89),(0,90),(0,91),(0,92),(0,93),(0,94),(0,95),(0,96),(0,97),(0,98),(0,99),(0,100),(0,101),(0,102),(0,103),(0,104),(0,105),(0,106),(0,107),(0,108),(0,109),(0,110),(0,111),(0,112),(0,113),(0,114),(0,115),(0,116),(0,117),(0,118),(0,119),(0,120),(0,121),(0,122),(0,123),(0,124),(0,125),(0,126),(0,127),(0,128),(0,129),(0,130),(0,131),(0,132),(0,133);
 /*!40000 ALTER TABLE `world_server` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -444,4 +444,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-03-14 11:10:50
+-- Dump completed on 2021-03-26  1:36:20
